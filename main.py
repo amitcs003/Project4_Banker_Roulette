d8ccdd7f47103fbe74749f64039edfab54c1beaa
Amit Govil <amit.cs03@gmail.com>
2022-06-10 17:24:01 +0800

    Added couple of approach to solve this problem

--- main.py
+++ main.py
@@ -10,12 +10,17 @@ names_string = input("Give me everybody's names, separated by a comma: ")
 names = names_string.split(", ")
 
 ## Could be done in 2 ways:
-# 1. Using lists
+
+# Approach 1. Using lists
 Count = len(names)
 random_position = random.randint(0, Count-1)
 
-print(f"{names[random_position]} is going to buy the meal today!")
+print(f"Approach 1: {names[random_position]} is going to buy the meal today!")
 
-# 2. Using random.choice() function
+# Approach 2. Using random.choice() function
+person_who_will_pay = random.choice(names)
+print(f" Approach 2: {person_who_will_pay} is going to buy the meal today!")
+
+
 
 
